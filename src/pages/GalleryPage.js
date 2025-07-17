91cdc114719e3486afc290e931e79260b891c067
Herbert Dunbar <herbertdunbar19@gmail.com>
2025-07-17 11:14:17 -0400

    update

---
+++ src/pages/GalleryPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GalleryPage = () => {
+  return (
+    <div className="p-8 text-center">
+      <h2 className="text-3xl font-bold mb-4">Gallery</h2>
+      <p>Here you can showcase your photography collection.</p>
+    </div>
+  );
+};
+
+export default GalleryPage;
