917601352c8ff1291ff692db119ebd0adf7e1270
Herbert Dunbar <herbertdunbar19@gmail.com>
2025-08-12 09:58:39 -0400

    updating js

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,16 @@
+// backend/db.js
+import 'dotenv/config';
 import pkg from 'pg';
+
 const { Pool } = pkg;
 
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: 5432
+  password: process.env.DB_PASS || null,
+  port: process.env.DB_PORT || 5432
 });
 
 export default pool;
+
